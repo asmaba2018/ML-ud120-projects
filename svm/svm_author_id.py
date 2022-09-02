70865418f726fd288bac18d812eccfc1008a9a57
Ahmed Sherif <eng_ahmed.sherif@yahoo.com>
2022-09-02 23:14:52 +0200

    SVM Quiz
    apply SVM quiz

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -22,6 +22,9 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+clf = svm.SVC(C=10000., kernel="rbf")
+C_list = [10., 100., 1000., 10000.]
 
 
 #########################################################
@@ -35,5 +38,20 @@ The Code Below solves that issue, So use this one
 
 # features_train = features_train[:int(len(features_train)/100)]
 # labels_train = labels_train[:int(len(labels_train)/100)]
+# t0 = time()
+clf.fit(features_train, labels_train)
+# print("Training Time:", round(time()-t0, 3), "s")
 
+# t0 = time()
+# clf.predict(features_test)
+# print("Training Time:", round(time()-t0, 3), "s")
+
+# for C_value in C_list:
+#     clf = svm.SVC(C=C_value, kernel="rbf")
+#     clf.fit(features_train, labels_train)
+#     accuracy = clf.score(features_test, labels_test)
+#     print(accuracy)
+
+accuracy = clf.score(features_test, labels_test)
+print(accuracy)
 #########################################################
